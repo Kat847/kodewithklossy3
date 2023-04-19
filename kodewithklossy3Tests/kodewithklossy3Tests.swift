8dcbbb5291dd20e9124e29649d3d4169eeaace80
Kat Barber <kbarber876@gmail.com>
2023-04-18 21:47:55 -0400

    Initial Commit

---
+++ kodewithklossy3Tests/kodewithklossy3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  kodewithklossy3Tests.swift
+//  kodewithklossy3Tests
+//
+//  Created by Barber, Kathryn Rose on 4/18/23.
+//
+
+import XCTest
+@testable import kodewithklossy3
+
+class kodewithklossy3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
