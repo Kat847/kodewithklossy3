8a2d0e775dabd9d23c5cdfdb6721e81a2e60e044
Kat Barber <kbarber876@gmail.com>
2023-04-18 21:50:00 -0400

    first commit

--- kodewithklossy3/ContentView.swift
+++ kodewithklossy3/ContentView.swift
@@ -2,15 +2,58 @@
 //  ContentView.swift
 //  kodewithklossy3
 //
-//  Created by Barber, Kathryn Rose on 4/18/23.
+//  Created by Barber, Kathryn Rose on 4/15/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstname = ""
+    @State private var lastname = ""
+    @State private var titleText = "Type your name into the text boxes"
+    @State private var presentAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 100){
+            
+            
+            //alert
+            Text(presentAlert ? "Presenting": "Dismissed")
+            
+            //first name
+            Text(titleText)
+                .font(.title)
+            TextField("Type your first name...", text: $firstname)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .border(Color.gray, width:1)
+            
+            Button("Submit First Name") {
+                titleText = "Hello, \(firstname)"
+                
+            }
+            .buttonStyle(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Button Style@*/DefaultButtonStyle()/*@END_MENU_TOKEN@*/)
+            
+            //last name
+            TextField("Type your last name...", text: $lastname)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .border(Color.gray, width:1)
+            
+            Button("Submit Last Name") {
+                presentAlert = true
+                
+            }
+            .buttonStyle(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Button Style@*/DefaultButtonStyle()/*@END_MENU_TOKEN@*/)
+            
+
+        }
+        .padding()
+        //alert
+        /*
+        .alert("Your new username is \(firstname)\(lastname)", isPresented: $presentAlert, actions: {})
+ */
+    
     }
 }
 
